8cfda1f9b839f7a7ed69fb5ccc23e8259ea8ef20
SLeBron <sophieclebron@gmail.com>
2022-09-20 21:26:45 -0600

    added tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -52,14 +52,56 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name,
+    productIds: req.body.product_id
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    attributes: ['tag_name'],
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(dbTagData => {
+    if(!dbTagData[0]) {
+      res.status(404).json({message: 'Tag with this id not found'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ 
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbTagData => {
+    if(!dbTagData) {
+      res.status(404).json({message: 'Tag with this id not found'});
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
